ea4acb8fd02fa67e6ce008e8600fb8dff6eedab7
Dan199312 <dan199312@gmail.com>
2022-04-09 15:05:38 +0200

    webpack.config modify with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
-const path = require('path');
-const HTMLWebpackPlugin = require ('html-webpack-plugin');
-const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ['./src/main.js', './src/style.scss'],
+  entry: ["./src/main.js", "./src/style.scss"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
   module: {
     rules: [
       {
-      test: /.scss$/,
-      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-    },
-  ],
+        test: /.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
-    new HTMLWebpackPlugin ({
-      template: './src/index.html',
-
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin(),
   ],
